5e3a73b906f347b2a62cd82051dfc03fc46fcc8d
adarsh-206 <adarshdhote8624@gmail.com>
2023-10-28 18:31:34 +0530

    BE Deployment

--- server/MenuManagement/MenuManagement/settings.py
+++ server/MenuManagement/MenuManagement/settings.py
@@ -9,12 +9,17 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -23,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l-lu!4&um19wmg1zr44q1$$=wmm2&25)awptpmlt75elsoii$c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -84,6 +89,10 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "5432",
     }
+}
+
+DATABASES = {
+    "default": dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
