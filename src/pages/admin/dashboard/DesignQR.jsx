1e057da08b0fc704a570405f9d9c006bd785d6b9
adarsh-206 <adarshdhote8624@gmail.com>
2023-10-28 16:51:40 +0530

    Setup backend code

--- src/pages/admin/dashboard/DesignQR.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DesignQR() {
-    return (
-        <div>DesignQR</div>
-    )
-}
-
-export default DesignQR
--- src/pages/admin/dashboard/PreviewMenu.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PreviewMenu() {
-    return (
-        <div>PreviewMenu</div>
-    )
-}
-
-export default PreviewMenu
