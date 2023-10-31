b6be10af48646fd23e863d02bb381ccf8b7e6bae
adarsh-206 <adarshdhote8624@gmail.com>
2023-10-31 14:23:16 +0530

    updated requirements.txt

--- server/MenuManagement/MenuManagement/urls.py
+++ server/MenuManagement/MenuManagement/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from authentication.views import UserRegistrationView, UserLoginView, UserDetailsView, LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     path('api/user/register', UserRegistrationView.as_view(), name='user-register'),
     path('api/user/login', UserLoginView.as_view(), name='user-register'),
     path('api/user/details', UserDetailsView.as_view(), name='user-details'),
