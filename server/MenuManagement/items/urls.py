d9ca1ad9a56439b1bfcfcab12e576e4a5b969f5e
Adarsh Dhote <66101827+adarsh-206@users.noreply.github.com>
2023-11-08 12:05:41 +0530

    Delete server directory

--- server/MenuManagement/items/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ItemList, ItemDetail
-
-urlpatterns = [
-    path('items/list', ItemList.as_view(), name='item-list'),
-    path('items/item-detail/<int:pk>', ItemDetail.as_view(), name='item-detail'),
-]
