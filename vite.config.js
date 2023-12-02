78437786a199c51c7ad5e508ba7ece739460a813
adarsh-206 <adarshdhote8624@gmail.com>
2023-12-02 18:05:02 +0530

    Build issue

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@mui/material'],
+  build: {
+    chunkSizeWarningLimit: 1000,
   },
-  base: '/Digital-Menu-App-Frontend/',
 })
